ba7e71366cc0374b1942324c3b896c65f3d938a7
Nipu Kumari <nipu.c7@gmail.com>
2025-11-02 21:09:21 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String args[])
+    {
+        RWOnly r1 = new RWOnly();
+//        r1.name = "IronMan";
+//        System.out.println(r1.name);
+//        error:-
+//        java: name has private access in com.driver.RWOnly
+
+        r1.setName("IronMan");
+        System.out.println(r1.getName());
+    }
   
 }
